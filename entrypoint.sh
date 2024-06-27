5102e5848df945d57de3970405925cc4e380ee98
Drixler Angelo <drixler97@gmail.com>
2024-06-27 20:03:16 +0800

    fix: set a custom migration routine when having a custom user model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 cd /app
+# Since I set a custom user model, I'll need to do this
+# Reference: https://stackoverflow.com/a/48476148
+sed -i "s|'django.contrib.admin'|#'django.contrib.admin'|g" app/settings.py
+sed -i "s|path('admin/', admin.site.urls)|#path('admin/', admin.site.urls)|g" app/urls.py
+python manage.py migrate
+sed -i "s|#'django.contrib.admin'|'django.contrib.admin'|g" app/settings.py
+sed -i "s|#path('admin/', admin.site.urls)|path('admin/', admin.site.urls)|g" app/urls.py
+python manage.py migrate
 python manage.py loaddata telecoms area_codes
 supervisord -c /app/supervisord.conf
