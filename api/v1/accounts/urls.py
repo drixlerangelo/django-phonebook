12f5797238b1c74846e1bc162c8b257ee0fa8f3e
Drixler Angelo Historillo <drixlerangelo.historillo@ingrammicro.com>
2024-08-28 20:50:09 +0800

    feat: add a route for retrieving the logged-in user

--- api/v1/accounts/urls.py
+++ api/v1/accounts/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='accounts.index'),
+
+    path(
+        'api/v1/accounts/me/',
+        views.AccountRetrieveView.as_view(),
+        name='accounts-retrieve'
+    ),
 ]
--- api/v1/accounts/views.py
+++ api/v1/accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import redirect
 from django.http import HttpRequest
 from app import settings
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from rest_framework import authentication, generics, permissions, throttling
+from .models import Account
+from .serializers import AccountSerializer
 
 def index(request: HttpRequest):
     if request.user.is_authenticated:
@@ -11,3 +15,14 @@ def index(request: HttpRequest):
                 return redirect('mfa_activate_totp')
         return redirect('swagger-ui')
     return redirect('account_login')
+
+class AccountRetrieveView(generics.RetrieveAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    authentication_classes = [OAuth2Authentication, authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    throttle_classes = [throttling.UserRateThrottle, throttling.AnonRateThrottle]
+    lookup_field = 'username'
+
+    def get_object(self):
+        return self.queryset.get(username=self.request.user.username)
